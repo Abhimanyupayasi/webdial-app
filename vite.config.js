6e954b71be37102f60186409b7b97be68f3014a4
Abhimanyu <abhimanyupayasi01@gmail.com>
2024-11-23 22:46:06 +0530

    hey

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: './', // Ensures correct paths in the `dist` folder
+//   build: {
+//     outDir: 'dist', // Default output folder
+//   },
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures correct paths in the `dist` folder
   build: {
-    outDir: 'dist', // Default output folder
+    outDir: 'dist', // Output directory
+    rollupOptions: {
+      input: './index.html',
+    },
   },
 });
+
